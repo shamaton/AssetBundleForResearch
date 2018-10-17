91885b2005faeb68a139cd11e2260340b9eb58dc
shamaton <shamaton77@gmail.com>
2018-10-17 19:08:56 +0900

    add

---
+++ Assets/CreateData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateData : ScriptableObject {
+  public string Message = "this_is_test_data";
+
+  [MenuItem("Assets/CreateData")]
+  static void CreateObject() {
+    CreateData data = CreateInstance<CreateData>();
+    string path = AssetDatabase.GenerateUniqueAssetPath("Assets/Resources/Data_"+ Application.unityVersion + ".asset");
+    AssetDatabase.CreateAsset(data, path);
+    AssetDatabase.Refresh();
+  }
+}
