adf81ec890d83e0cf3181b3827c00e0ea1878a4e
shamaton <shamaton77@gmail.com>
2018-10-17 19:21:33 +0900

    add

--- Assets/Editor/CreateData.cs
+++ Assets/Editor/CreateData.cs
@@ -6,11 +6,24 @@ using UnityEditor;
 public class CreateData : ScriptableObject {
   public string Message = "this_is_test_data";
 
-  [MenuItem("Assets/CreateData")]
+  [MenuItem("Shamaton/CreateData")]
   static void CreateObject() {
     CreateData data = CreateInstance<CreateData>();
     string path = AssetDatabase.GenerateUniqueAssetPath("Assets/Resources/Data_"+ Application.unityVersion + ".asset");
     AssetDatabase.CreateAsset(data, path);
     AssetDatabase.Refresh();
+  }
+
+}
+
+public class CreateBundler {
+  [MenuItem("Shamaton/CreateBundle")]
+  static void CreateBundle() {
+    Debug.Log("create bundle");
+    BuildPipeline.BuildAssetBundles(
+      "Assets/AssetBundles",
+      BuildAssetBundleOptions.ChunkBasedCompression,
+      BuildTarget.Android
+    );
   }
 }
